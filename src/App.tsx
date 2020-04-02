fdcbec82d06d2feaf3153e6bf2d7334699baf4e7
Nicolas Bazille <oltodo@msn.com>
2020-04-02 23:39:25 +0200

    Change default background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Playground from "./Playground";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    background: {
+      default: "#282B40",
+    },
+    type: "dark",
+  },
+});
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Playground />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
 import { Provider } from "react-redux";
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import * as serviceWorker from "./serviceWorker";
 import store from "./redux/store";
-
-const theme = createMuiTheme({
-  palette: {
-    type: "dark",
-  },
-});
 
 const render = () => {
   const App = require("./App").default;
 
   ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </Provider>
-    </AppContainer>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById("root"),
   );
 };
